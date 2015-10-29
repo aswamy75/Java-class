eda6938c7aea863ac00da9d3e1bfee040be0d6d1
20597782 <20597782@RIC1LWPB9T8GB.GHS.ONEACS.COM>
2015-10-28 22:20:45 -0400

    additional commits

---
+++ Java training/src/SecondJavaprogram.java
@@ -0,0 +1,9 @@
+
+public class SecondJavaprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is to do additional check in to GIT");
+	}
+
+}
