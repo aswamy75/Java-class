4d294da69f861546ecf07e35bb4e3e26964afabe
aswamy75 <rojaa@hotmail.com>
2015-10-29 13:19:54 -0400

    trial

--- Java class/src/ThirdProgram.java
+++ Java class/src/ThirdProgram.java
@@ -3,6 +3,6 @@ public class ThirdProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello HOw are u???? ");
-		System.out.println("ThirdProgram.main()");
+		System.out.println("Let us check whether it fetches last update");
 	}
 }
